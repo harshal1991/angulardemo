16261efd743a73565282689c982eb847e67fb1f9
harshal1991 <harshal.prajapati37@gmail.com>
2017-10-23 09:51:34 -0400

    First commit

---
+++ Scripts/script.js
@@ -0,0 +1,129 @@
+﻿
+/// <reference path="angular.js" />
+/// <reference path="angular-route.js" />
+
+
+
+
+//var app = angular.module("Demo", ["ngRoute"])
+var app = angular.module("Demo", ["ui.router"])
+
+                    .config(function ($routeProvider, $locationProvider) {
+                        $routeProvider.caseInsensitiveMatch = true;
+                        $routeProvider
+                         .when("/home", {
+                             templateUrl: "Templates/Home.html",
+                             controller: "homecontroller"
+                         })
+                           .when("/courses", {
+                               templateUrl: "Templates/courses.html",
+                               controller: "coursecontroller"
+                           })
+                           .when("/students", {
+                               templateUrl: "Templates/Students.html",
+                               controller: "Studentcontroller",
+                               controllerAs: "StudentCtrl",
+                               resolve: {
+                                   studentList: function ($http) {
+                                       return $http.get("StudentService.asmx/GetCityName")
+                                                .then(function (response) {
+                                                    return response.data;
+                                                })
+
+                                   }
+                               }
+
+                           })
+                            .when("/students/:CityId", {
+                                templateUrl: "Templates/StudentDetail.html",
+                                controller: "StudentDetailcontroller"
+                            })
+                            .when("/StudentSearch/:CityName?", {
+                                templateUrl: "Templates/studentsSearch.html",
+                                controller: "StudentSearchcontroller",
+                                controllerAs: "StudentSearchCtrl"
+                            })
+                         .otherwise({
+                             redirectTo: "/home"
+                         })
+
+                        $locationProvider.html5Mode(true);
+
+                    })
+
+                    .controller("homecontroller", function ($scope) {
+                        $scope.message = "Home Page";
+                    })
+                    .controller("coursecontroller", function ($scope) {
+                        $scope.courses = ["ASP.NET", "C#", "JAVA", "PHP", "ANDROID"];
+                    })
+                    .controller("Studentcontroller", function (studentList, $http, $route, $scope, $location) {
+                        var vm = this;
+
+                        vm.searchstudent = function () {
+                            if (vm.CityName) {
+                                $location.url("/StudentSearch/" + vm.CityName)
+                            }
+                            else {
+                                $location.url("/StudentSearch")
+                            }
+                        }
+
+                        vm.Reloadf = function () {
+                            $route.reload();
+                        }
+
+                        $scope.$on("$locationChangeStart", function (event, next, current) {
+                            if (!confirm("Are you sure navigate" + next)) {
+                                event.preventDefault();
+                            }
+                        });
+
+                        vm.students = studentList;
+
+
+
+                    })
+
+                        .controller("StudentDetailcontroller", function ($scope, $http, $routeParams) {
+
+                            $http({
+                                url: "StudentService.asmx/GetCityNameById",
+                                params: { CityId: $routeParams.CityId },
+                                method: "get"
+
+                            })
+                                .then(function (response) {
+                                    $scope.student = response.data;
+
+                                })
+
+                        })
+
+                .controller("StudentSearchcontroller", function ($http, $routeParams) {
+                    var vm = this;
+                    if ($routeParams.CityName) {
+
+                        $http({
+                            url: "StudentService.asmx/GetCityNameByName",
+                            params: { CityName: $routeParams.CityName },
+                            method: "get"
+
+                        })
+                               .then(function (response) {
+                                   vm.students = response.data;
+
+                               })
+
+
+                    }
+                    else {
+                        $http.get("StudentService.asmx/GetCityName")
+                       .then(function (response) {
+                           vm.students = response.data;
+
+                       })
+                    }
+                })
+
+
---
+++ StudentService.asmx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data.SqlClient;
+using System.Web.Script.Serialization;
+using System.Configuration;
+
+
+namespace Angularextra
+{
+    /// <summary>
+    /// Summary description for StudentService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class StudentService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public void GetCityName()
+        {
+            List<student> liststudent = new List<student>();
+            string cs = ConfigurationManager.ConnectionStrings["AccessSoftware"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("select * from tableCity", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    student stu = new student();
+                    stu.CityId = Convert.ToInt32(rdr["CityId"]);
+                    stu.CityName = rdr["CityName"].ToString();
+                    stu.CityCode = rdr["CityCode"].ToString();
+                    liststudent.Add(stu);
+                }
+
+
+            }
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(liststudent));
+        }
+
+        //------WITH ID------------//
+        [WebMethod]
+        public void GetCityNameById(int CityId)
+        {
+            student stud = new student();
+            string cs = ConfigurationManager.ConnectionStrings["AccessSoftware"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("select * from tableCity where CityId=@CityId", con);
+
+                SqlParameter param = new SqlParameter()
+                {
+                    ParameterName = "@CityId",
+                    Value = CityId
+
+                };
+                cmd.Parameters.Add(param);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+
+                    stud.CityId = Convert.ToInt32(rdr["CityId"]);
+                    stud.CityName = rdr["CityName"].ToString();
+                    stud.CityCode = rdr["CityCode"].ToString();
+
+                }
+
+
+            }
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(stud));
+        }
+
+
+        //------WITH Name------------//
+        [WebMethod]
+        public void GetCityNameByName(string CityName)
+        {
+            
+            List<student> liststudent = new List<student>();
+            string cs = ConfigurationManager.ConnectionStrings["AccessSoftware"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("select * from tableCity where CityName like @CityName", con);
+
+                SqlParameter param = new SqlParameter()
+                {
+                    ParameterName = "@CityName",
+                    Value = CityName + "%"
+
+                };
+                cmd.Parameters.Add(param);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    student stud = new student();
+                    stud.CityId = Convert.ToInt32(rdr["CityId"]);
+                    stud.CityName = rdr["CityName"].ToString();
+                    stud.CityCode = rdr["CityCode"].ToString();
+                    liststudent.Add(stud);
+
+                }
+
+
+            }
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(liststudent));
+        }
+    }
+}
+
